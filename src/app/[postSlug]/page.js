5e11c77daee3569c1affa7d638e739313605354c
Duane Evans <duanesevans@gmail.com>
2025-01-04 15:13:13 -0800

    ex3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,6 +3,18 @@ import BlogHero from "@/components/BlogHero"
 import { MDXRemote } from "next-mdx-remote/rsc"
 import styles from "./postSlug.module.css"
 import { loadBlogPost } from "@/helpers/file-helpers"
+import { BLOG_TITLE } from "@/constants"
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = await params
+  const {
+    frontmatter: { title, abstract },
+  } = await loadBlogPost(postSlug)
+  return {
+    title,
+    description: `${abstract} • ${BLOG_TITLE}`,
+  }
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = await params
